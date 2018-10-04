b50ea9dfa397fb1191ff787a8d73ee692b72286a
sdiperi90 <sirgasheva17@gmail.com>
2018-10-03 23:28:11 -0400

    set up conditions for rock pick

--- play.js
+++ play.js
@@ -11,34 +11,43 @@ document.addEventListener("DOMContentLoaded", function() {
     const PICKBTNS = document.querySelector("#player1-btn");
     var active = false;
 
+    let PLAYER1PICK;
+    let COMPUTERPICK;
+
     ROCK.addEventListener("click", function() {
         if (!active) {
             active = ROCK;
             BOARD.appendChild(ROCK);
+            PLAYER1PICK = "rock";
         } else {
             PICKBTNS.appendChild(active);
             active = ROCK;
             BOARD.appendChild(ROCK);
+            PLAYER1PICK = "rock";
         }
     });
     PAPER.addEventListener("click", function() {
         if (!active) {
             active = PAPER;
             BOARD.appendChild(PAPER);
+            PLAYER1PICK = "paper";
         } else {
             PICKBTNS.appendChild(active);
             active = PAPER;
             BOARD.appendChild(PAPER);
+            PLAYER1PICK = "paper";
         }
     });
     SCISSORS.addEventListener("click", function() {
         if (!active) {
             active = SCISSORS;
             BOARD.appendChild(SCISSORS);
+            PLAYER1PICK = "scissors";
         } else {
             PICKBTNS.appendChild(active);
             active = SCISSORS;
             BOARD.appendChild(SCISSORS);
+            PLAYER1PICK = "scissors";
         }
     });
 });
@@ -55,20 +64,28 @@ const PLAYBTN = document.querySelector(".start-game-btn");
 
 function play() {
     console.log("SAIDA");
-    var computerPick = Math.floor(Math.random() * 3);
-    if (computerPick < 1) {
-        computerPick = COMPUTERROCK;
+    var computerRandomPick = Math.floor(Math.random() * 3);
+    if (computerRandomPick < 1) {
+        computerRandomPick = COMPUTERROCK;
         COMPUTERBOARD.appendChild(COMPUTERROCK);
-    } else if (computerPick > 1 && computerPick < 2) {
-        computerPick = COMPUTERPAPER;
+        COMPUTERPICK = "rock";
+    } else if (computerRandomPick > 1 && computerPick < 3) {
+        computerRandomPick = COMPUTERPAPER;
         COMPUTERBOARD.appendChild(COMPUTERPAPER);
+        COMPUTERPICK = "paper";
     } else {
-        computerPick = COMPUTERSCISSORS;
+        computerRandomPick = COMPUTERSCISSORS;
         COMPUTERBOARD.appendChild(COMPUTERSCISSORS);
+        COMPUTERPICK = "scissors";
     }
 
     var compare = function(choice1, choice2) {
         if (choice1 === choice2) {
+            return "TIE";
+        } else if ((ROCK, COMPUTERPAPER)) {
+            return "PAPER WRAP ROCK";
+        } else if ((ROCK, SCISSORS)) {
+            return "ROCK SMASHES SCISSOR";
         }
     };
 }
